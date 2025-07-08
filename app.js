947d1838a977ca4573a783ed3083cecdf582ec1a
TomasManazza80 <tomas.manazza19@gmail.com>
2025-07-08 16:57:00 -0300

    back added fix 2

--- app.js
+++ app.js
@@ -40,14 +40,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-app.use(`/`, indexRouter);
-app.use(`/`, usersRouter);
-app.use(`/product`, productRouter);
- 
-app.use(`/payment`, paymentRouter);
-
-app.use(`/products`, productRouter);
-app.use(`/boughtProduct`, productBought);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/products', productRouter);
+app.use('/payment', paymentRouter);
+app.use('/boughtProduct', productBought);
 
 
 // catch 404 and forward to error handler
